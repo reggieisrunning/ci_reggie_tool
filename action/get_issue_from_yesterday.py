442c4a2ccc1ff05624319b16d3e4a55b03123fa2
reggiezhou <reggiezhou@tencent.com>
2022-06-17 16:14:38 +0800

    bugfix: 修复一些代码错误

--- action/get_issue_from_yesterday.py
+++ action/get_issue_from_yesterday.py
@@ -43,4 +43,3 @@ if __name__ == '__main__':
                 fd.write("    </tr>\n")
                 print(issue_item)
         fd.write("</body>")
-
--- action/get_issue_of_iccengan.py
+++ action/get_issue_of_iccengan.py
@@ -8,7 +8,7 @@ def judge_is_my(issue, label_list):
 
     for label_item in label_list:
         try:
-            #if re.search(label_item, issue["issue_labels"]) and issue["issue_assignee"] in [ None, "zanyzhao", "iccengan"]:
+            # if re.search(label_item, issue["issue_labels"]) and issue["issue_assignee"] in [ None, "zanyzhao", "iccengan"]:
             #    print(item)
             #    break
             if re.search("iccengan", str(issue["issue_assignee"])):
@@ -31,7 +31,3 @@ if __name__ == '__main__':
     issue_list = github_api.get_github_issue_list(owner="Tencent", repo="bk-ci")
     for item in issue_list:
         judge_is_my(item, label_list)
-
-
-
-
